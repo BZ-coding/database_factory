0542dd621fa1ef0eb3ea219a8a48c69eaf85eff3
BZ <435878393@qq.com>
2023-08-19 03:03:40 +0800

    更新init里的import方式

--- __init__.py
+++ __init__.py
@@ -1,6 +1,18 @@
+from glob import glob
+from keyword import iskeyword
+from os.path import dirname, join, splitext, split
+
 from ._factory import DataBaseFactory
 
-from ._database_pymongo import DataBasePyMongo
-from ._database_sqlite3 import DataBaseSqlite3
-
 __all__ = ['DataBaseFactory']
+
+basedir = dirname(__file__)
+
+for name in glob(join(basedir, '*.py')):
+    module = splitext(split(name)[-1])[0]
+    if module.startswith('_') and module.isidentifier() and not iskeyword(module):
+        try:
+            __import__(__name__ + '.' + module)
+            print(f"import {__name__ + '.' + module} success.")
+        except Exception as e:
+            pass
